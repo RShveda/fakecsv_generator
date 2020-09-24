e24e875fd7d7d72a03b139919c8efc9e2409e693
Roman Shveda <rshveda@gmail.com>
2020-09-24 15:48:59 +0300

    allowed host fixed

--- fakecsv_generator/settings.py
+++ fakecsv_generator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_h^6usgntj@0!$9sjc5m-^#10w2mda
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["https://csv-generator.herokuapp.com/", '127.0.0.1']
+ALLOWED_HOSTS = ["csv-generator.herokuapp.com", '127.0.0.1']
 
 
 # Application definition
