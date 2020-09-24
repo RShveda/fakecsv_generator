f92c8bac24ed34e01690597831b05ffc8dfb51d3
Roman Shveda <rshveda@gmail.com>
2020-09-24 15:09:04 +0300

    basic implementation of csv fake data generator via faker lib

--- schemas/csv_generator.py
+++ schemas/csv_generator.py
@@ -1,16 +1,23 @@
 from django.core.files import File
+from .models import Schema
 import os
 from django.conf import settings
+from faker import Faker
+import csv
 
 class CsvFaker:
 
     def make_file(schema, rows):
-        with open(str(settings.MEDIA_ROOT) + "\datasets\\" + schema + str(rows) + ".txt", 'w') as f:
+        with open(str(settings.MEDIA_ROOT) + "/datasets/" + schema + str(rows) + ".csv", 'w', newline='') as f:
             myfile = File(f)
-            myfile.write('Hello World ' + schema)
+            myfile.write(CsvFaker.generate_data(schema, rows))
         myfile.closed
         f.closed
         return myfile.name
 
-    def generate_data(self):
-        pass
+    def generate_data(schema, rows):
+        schema = Schema.objects.get(name=schema)
+        fake = Faker()
+        fake.set_arguments('range', {'min_value': 0, 'max_value': 100})
+        csv = fake.csv(header=('Name', 'Address', 'Favorite Color'), data_columns=('{{name}}', '{{job}}', '{{pyint:range}}'), num_rows=int(rows), include_row_ids=False)
+        return (csv)
