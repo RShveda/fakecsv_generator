6ca09b4b5ca244552bd95a8272f733c5852de0ab
Roman Shveda <rshveda@gmail.com>
2020-09-30 11:11:26 +0300

    UI improvements

--- fakecsv_generator/settings.py
+++ fakecsv_generator/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
-    'schemas',
     'cloudinary',
+    'bootstrap4',
+    'schemas',
 ]
 
 MIDDLEWARE = [
--- schemas/models.py
+++ schemas/models.py
@@ -58,6 +58,9 @@ class DataSet(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     url = models.FileField(blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id']
+
     def __str__(self):
         return self.title
 
